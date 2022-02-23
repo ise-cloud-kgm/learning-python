6a39ed66783f72343182e4376788e83a3ca0754e
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2022-02-23 14:01:44 +0300

    Add task 1.6

---
+++ Chapter_1/Task_1_6.py
@@ -0,0 +1,6 @@
+number = int(input('Введите целое число: '))
+
+if number % 3 == 0:
+    print(f'Число {number} делится на 3 без остатка')
+else:
+    print(f'Число {number} не делится на 3 без остатка')
