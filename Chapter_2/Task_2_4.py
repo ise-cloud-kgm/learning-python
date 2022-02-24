7fa8533289579c5f534e8934542f05ca44548a1f
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2022-02-24 15:36:56 +0300

    Add task 2.4

---
+++ Chapter_2/Task_2_4.py
@@ -0,0 +1,9 @@
+list1 = [1, 2, 3, 4, 5, 6]
+list2 = [1, 2, 3, 4, 5]
+
+print(f'\nlist1 = {list1}\nlist2 = {list2}\n')
+
+if list1 == list2:
+    print(f'list1 = list2')
+else:
+    print(f'Списки list1 и list2 не равны')
